26f4a14db9d252d28c3264f9dd7b182a3ccc65a4
Berkcan Karabulut <46574515+berkcankarabulut@users.noreply.github.com>
2025-09-30 14:29:19 +0300

    Initial commit

---
+++ src/main/java/com/BerkcanKarabulut/StudentApp/controller/StudentController.java
@@ -0,0 +1,39 @@
+package com.BerkcanKarabulut.StudentApp.controller;
+
+import com.BerkcanKarabulut.StudentApp.model.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+	List<Student> students=new ArrayList<>(List.of(
+			new Student(1,"Berkcan","Java"),
+			new Student(2,"Cagdas","Blockchain")
+			));
+	
+	@GetMapping("csrf-token")
+	public CsrfToken getCsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
+	}
+	
+	
+	
+	
+	
+	@GetMapping("students")
+	public List<Student> getStudents(){
+		return students;
+	}
+	@PostMapping("students")
+	public void addStudent(@RequestBody Student student) {
+		students.add(student);
+	}
+}
